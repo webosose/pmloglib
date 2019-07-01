d559c8fbef9c609e682df1d13063e4a72133adb8
pooja.jumana <pooja.jumanal@lge.com>
2019-07-01 13:26:03 +0530

    Fix coverity issues
    :Release Notes:
    Fix coverity issues for no return value check
    
    :Detailed Notes:
    - The return value of function g_strlcpy() is not checked
      Fixed it by prefixing it with type void
    
    :Testing Performed:
    Fixed and built on official build 88
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-80595] 238523 Unchecked return value from library (pmloglib)
    
    Change-Id: Ic6bf4c1e7b4fed9003d6048e800a5f48fdd0654d

--- src/PmLogLib.c
+++ src/PmLogLib.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2007-2018 LG Electronics, Inc.
+// Copyright (c) 2007-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -287,7 +287,7 @@ void GetPidStr(PmLogContext_ *context,char *ptidStr,long int ptidStrLen)
             snprintf(ptidStr, ptidStrLen, "[%d]", (int) pid);
         }
     } else {
-        g_strlcpy(ptidStr, "[]", ptidStrLen);
+        (void) g_strlcpy(ptidStr, "[]", ptidStrLen);
     }
 }
 
